fe4549c9e16160e914bd8f77c3ebe118ce51aaf3
Vitor Bortolotti <bortolotti.vitor@gmail.com>
2021-01-21 22:16:20 -0300

    Basic error handling when user cancels input

--- src/index.ts
+++ src/index.ts
@@ -15,22 +15,31 @@ interface Instance {
 
 (async () => {
 
-  // Fetch list of projects
-  const projects = await fetchProjects()
+  try {
+    // Fetch list of projects
+    const projects = await fetchProjects()
 
-  // Prompt user to select one project
-  const selectedProject = await promptSelectProject(projects)
+    // Prompt user to select one project
+    const selectedProject = await promptSelectProject(projects)
 
-  // Fetch list of instances
-  const instances = await fetchInstances(selectedProject)
+    // Fetch list of instances
+    const instances = await fetchInstances(selectedProject)
 
-  // Prompt user to select one instance
-  const selectedInstance = await promptSelectInstance(instances)
+    // Prompt user to select one instance
+    const selectedInstance = await promptSelectInstance(instances)
 
-  // Prompt user to input port
-  const selectedPort = await promptPort()
+    // Prompt user to input port
+    const selectedPort = await promptPort()
 
-  startProxy(selectedInstance, selectedPort)
+    startProxy(selectedInstance, selectedPort)
+
+  } catch (e) {
+    if (e === "halt") {
+      console.log('Bye!')
+    } else {
+      console.error(e)
+    }
+  }
 
 })();
 
@@ -62,6 +71,8 @@ async function promptSelectProject(projects: Project[]): Promise<Project> {
     format: (selected: string) => projects.find((p: Project) => p.projectId === selected)
   });
 
+  if (!promptResponse.project) throw 'halt'
+
   return promptResponse.project
 }
 
@@ -77,6 +88,8 @@ async function promptSelectInstance(instances: Instance[]): Promise<Instance> {
     format: (selected: string) => instances.find((i: Instance) => i.connectionName === selected)
   });
 
+  if (!promptResponse.instance) throw 'halt'
+
   return promptResponse.instance
 }
 
@@ -84,8 +97,11 @@ async function promptPort(): Promise<string> {
   const promptResponse = await prompts({
     type: 'number',
     name: 'port',
-    message: 'Select port'
+    message: 'Input port',
+    initial: '5432'
   });
+
+  if (!promptResponse.port) throw 'halt'
 
   return promptResponse.port
 }
@@ -93,12 +109,12 @@ async function promptPort(): Promise<string> {
 function startProxy(instance: Instance, port: string) {
   const args = `-instances=${instance.connectionName}=tcp:${port}`
 
-  console.log("Running command: gcloud", args)
+  console.log("Running command: ", args)
 
   const child = spawn('cloud_sql_proxy', [args]);
 
   // use child.stdout.setEncoding('utf8'); if you want text chunks
-  child.stderr.on('data', (chunk: Buffer) => {
+  child.stderr.setEncoding('utf8').on('data', (chunk: Buffer) => {
     // data from standard output is here as buffers
     console.log(chunk.toString())
   });
@@ -111,6 +127,9 @@ function startProxy(instance: Instance, port: string) {
 async function commandExec(command: string): Promise<string> {
   return new Promise((resolve, reject) => {
     exec(command, (err: Error, stdout: string, stderr: string) => {
+      if (err) {
+        throw err;
+      }
 
       resolve(stdout)
     });
